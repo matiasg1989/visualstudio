1219789b5b41d46e4894d4f66122ecc0479771a1
matiasg1989 <matiasg1989@gmail.com>
2014-04-29 17:55:33 -0300

    217.

---
+++ Sportsore2/SportsStore.Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using SportsStore.Domain.Entities;
+using System.Data.Entity;
+namespace SportsStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext()
+            : base("SportsScore")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
